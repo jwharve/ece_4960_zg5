9da48842754bb5c7948901b8823adc8ec0db2f6f
Joshua Harvey <jwharve@g.clemson.edu>
2019-02-25 23:35:33 +0000

    serial woring - pi

--- serial/serial.c
+++ serial/serial.c
@@ -1,15 +1,30 @@
 #include <wiringPi.h>
 #include <wiringSerial.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(void)
+
+int main(int argc, char * argv[])
 {
 	int uart_port;
-	
+	int i;
+	unsigned int num;
+	char * rover;
+
 	uart_port = serialOpen("/dev/ttyS0",9600);
-	
+
 	wiringPiSetup();
-	
-	serialPutchar(uart_port,'b');
-	
+
+	num = (unsigned int)atoi(argv[1]);
+	printf("pos = %d\n",num);
+	rover = (char *)&num;
+
+	for (i = 0; i < 4; i++)
+	{
+		serialPutchar(uart_port,rover[i]);
+		printf("%x ",rover[i]);
+	}
+	printf("\n");
+
 	serialClose(uart_port);
 }
