5cb57e7b74e8a58785340463bf62d05f577bb7b5
graysonglanton <gglanto@g.clemson.edu>
2019-03-25 17:32:15 -0400

    specificPos update

--- source/specificPos.c
+++ source/specificPos.c
@@ -23,7 +23,7 @@ int main(int argc, char * argv[])
 	locF = fopen("current.loc","r");
 
 	fscanf(locF,"%f %f %c",p1.x,p1.y,prevTool);
-	p1.z = 0;
+	p1.z = DRAW_HEIGHT;
 	p1.theta = 0;
 	
 	uart_port = serialOpen("/dev/ttyS0",9600);
@@ -34,16 +34,22 @@ int main(int argc, char * argv[])
 	p2.theta = atof(argv[3]);
 	
 	
-	move( , , 1, uart_port);
+	move(p1,p2,1,uart_port);
 	
-	struct gLine line[2];
-	FILE * fptr;
-	unsigned long num = 0;
-//	float z[2];
-	int readSuccess;
-	int uart_port;
-	char filename[FILE_NAME_LEN];
-	filename[0] = 0;
+// 	struct gLine line[2];
+// 	FILE * fptr;
+// 	unsigned long num = 0;
+// //	float z[2];
+// 	int readSuccess;
+// 	int uart_port;
+// 	char filename[FILE_NAME_LEN];
+// 	filename[0] = 0;
+
+
+	fclose(locF);
+	locF = fopen("current.loc","w");
+	fprintf(locF,"%f %f %c\n", p2.x,p2.y,prevTool);
+	fclose(locF);
 
 
 
