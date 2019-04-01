d1f6897911b968a240aa87cf5f15724e291c0617
Joshua Harvey <jwharve@g.clemson.edu>
2019-04-01 14:20:43 -0400

    update spos

--- source/specificPos.c
+++ source/specificPos.c
@@ -5,7 +5,6 @@
 
 int zero_step;
 
-
 int main(int argc, char * argv[])
 {
 
@@ -16,42 +15,33 @@ int main(int argc, char * argv[])
 
 	float nextX, nextY;
 
+	char * locLine = NULL;
+	size_t sizeLine;
+	
+	// Initialization
 	wiringPiSetup();
 	initGlobal();
-
-	locF = fopen("current.loc","r");
-
-	fscanf(locF,"%f %f %c",p1.x,p1.y,prevTool);
-	p1.z = DRAW_HEIGHT;
-	p1.theta = 0;
-
 	uart_port = serialOpen("/dev/ttyS0",9600);
+	
+	// Initalize P1
+	locF = fopen("current.loc","r");
+	getline(&locLine,&sizeLine,locF);
+	sscanf(locLine,"%f %f %c",&(p1.x),&(p1.y),&(p1.tool));
+	p1.z = DRAW_HEIGHT;
+	fclose(locF);
+	free(locLine);
 
+	// Initialize P2
 	p2.x = atof(argv[1]);
 	p2.y = atof(argv[2]);
 	p2.z = p1.z;
 	p2.theta = atof(argv[3]);
 	
-	
+	// Move
 	move(p1,p2,1,uart_port);
-	
-// 	struct gLine line[2];
-// 	FILE * fptr;
-// 	unsigned long num = 0;
-// //	float z[2];
-// 	int readSuccess;
-// 	int uart_port;
-// 	char filename[FILE_NAME_LEN];
-// 	filename[0] = 0;
 
-
-	fclose(locF);
+	// Write out position
 	locF = fopen("current.loc","w");
 	fprintf(locF,"%f %f %c\n", p2.x,p2.y,prevTool);
 	fclose(locF);
-
-
-
-
-
 }
